d9ee106124088ba8cd198936afd56974bf5332b3
Rémi Brun <remi.brun@appsolu.net>
2018-07-10 16:25:16 +0200

    Prise en charge nouvelle version angular

--- src/datepicker.module.ts
+++ src/datepicker.module.ts
@@ -3,13 +3,18 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 
 import { DatepickerComponent } from './datepicker.component';
-import {MaterialModule} from '@angular/material';
+import {MatIconModule} from '@angular/material';
+//import {MaterialModule} from '@angular/material';
+
 
 export * from './datepicker.component';
 
 @NgModule({
   declarations: [ DatepickerComponent ],
   exports: [ DatepickerComponent ],
-  imports: [ CommonModule, FormsModule, ReactiveFormsModule, MaterialModule, ]
+  imports: [ CommonModule, FormsModule, ReactiveFormsModule,
+     //MaterialModule, 
+    MatIconModule,
+ ]
 })
 export class DatepickerModule { }
